92f54dd62e75170b0cd0fcce1ad8a56e711d9473
liao <liao@liao-PC>
2016-03-17 01:40:20 +0800

    d

--- src/com/yun/controler/UserControler.java
+++ src/com/yun/controler/UserControler.java
@@ -1,12 +1,22 @@
 package com.yun.controler;
 
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.yun.dao.BaseDao;
 @Controller
 public class UserControler {
+	@Resource
+	private BaseDao baseDao;
 	@RequestMapping("test")
 	public String inputProduct() {
+		List<?>list=baseDao.getList("select * from TB_SYS_USER t");
+		System.out.println(list);
 		return "index";
 	}
 	
