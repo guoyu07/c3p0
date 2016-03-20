fd06f0dab3d82c4177bcaace436a43a4de8af4ac
subzero1 <1516316152@qq.com>
2016-03-20 12:42:25 +0800

    dao modigy

--- src/com/base/dao/BaseDao.java
+++ src/com/base/dao/BaseDao.java
@@ -3,17 +3,45 @@ package com.base.dao;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.dao.DataAccessException;
 
 public interface BaseDao {
+	
+	/**
+	 * Issue a single SQL execute, typically a DDL statement.
+	 * @param sql static SQL to execute
+	 * @throws DataAccessException if there is any problem
+	 */
+	public void execute(String sql) throws DataAccessException;
 
-	List<?> getList(String sql);
+	/**
+	 * Issue a single SQL update operation (such as an insert, update or delete statement).
+	 * @param sql static SQL to execute
+	 * @return the number of rows affected
+	 * @throws DataAccessException if there is any problem.
+	 */
+	public int update(String sql) throws DataAccessException;
+	/**
+	 * Execute a batch using the supplied SQL statement with the batch of supplied arguments.
+	 * @param sql the SQL statement to execute
+	 * @param batchArgs the List of Object arrays containing the batch of arguments for the query
+	 * @return an array containing the numbers of rows affected by each update in the batch
+	 */
+	public int[] batchUpdate(String... sql) throws DataAccessException;
+	/**
+	 * Execute a batch using the supplied SQL statement with the batch of supplied arguments.
+	 * @param sql the SQL statement to execute
+	 * @param batchArgs the List of Object arrays containing the batch of arguments for the query
+	 * @return an array containing the numbers of rows affected by each update in the batch
+	 */
+	public int[] batchUpdate(List<String> sqls);
+
+	public Map<String, Object> queryForMap(String sql);
 	
-	public int update(String sql);
+	public List<Map<String, Object>> queryForList(String sql) throws DataAccessException;
 	
-	public int[] batchUpdate(List<String>sqls);
+	public int queryForCount(String sql) throws DataAccessException;
 	
-	public List<String> getListMap(List<String>sqls);
-	
-	public Map  getMap(String sql);
+	public  String quertForValue(String sql,String key);
+
 }
--- src/com/base/dao/impl/BaseDaoImpl.java
+++ src/com/base/dao/impl/BaseDaoImpl.java
@@ -5,34 +5,57 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import com.base.dao.BaseDao;
 
 @Service
-public class BaseDaoImpl implements BaseDao{
-	
+public class BaseDaoImpl implements BaseDao {
+
 	@Resource
 	JdbcTemplate jdbcTemplate;
-	public List getList(String sql){
-		return this.jdbcTemplate.queryForList(sql);
-		
+
+	public int[] batchUpdate(String... sql) throws DataAccessException {
+		return this.batchUpdate(sql);
 	}
+
 	public int[] batchUpdate(List<String> sqls) {
-		// TODO Auto-generated method stub
-		return null;
+
+		return this.batchUpdate(sqls);
 	}
-	public List<String> getListMap(List<String> sqls) {
-		// TODO Auto-generated method stub
-		return null;
+
+	public void execute(String sql) throws DataAccessException {
+
+		this.execute(sql);
+
 	}
-	public Map getMap(String sql) {
-		// TODO Auto-generated method stub
-		return null;
+
+	public String quertForValue(String sql, String key) {
+
+		return this.quertForValue(sql, key);
 	}
-	public int update(String sql) {
-		// TODO Auto-generated method stub
-		return 0;
-	};
+
+	public int queryForCount(String sql) throws DataAccessException {
+
+		return this.queryForCount(sql);
+	}
+
+	public List<Map<String, Object>> queryForList(String sql)
+			throws DataAccessException {
+
+		return this.queryForList(sql);
+	}
+
+	public Map<String, Object> queryForMap(String sql) {
+
+		return this.queryForMap(sql);
+	}
+
+	public int update(String sql) throws DataAccessException {
+
+		return this.update(sql);
+	}
+
 }
